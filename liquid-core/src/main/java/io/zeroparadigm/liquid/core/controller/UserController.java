1b3cd7b5770b25bfa39e02636c3e85e51c7e07db
Bucky Lee <2209203187@qq.com>
2022-12-01 10:58:36 +0800

    feat: sync

--- liquid-core/src/main/java/io/zeroparadigm/liquid/core/controller/UserController.java
+++ liquid-core/src/main/java/io/zeroparadigm/liquid/core/controller/UserController.java
@@ -17,12 +17,10 @@
 
 package io.zeroparadigm.liquid.core.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import io.zeroparadigm.liquid.common.constants.StorageConsts;
 import io.zeroparadigm.liquid.common.dto.Result;
 import io.zeroparadigm.liquid.common.enums.ServiceStatus;
 import io.zeroparadigm.liquid.common.exceptions.annotations.WrapsException;
@@ -30,7 +28,6 @@ import io.zeroparadigm.liquid.core.dao.UserDao;
 import io.zeroparadigm.liquid.core.dao.entity.User;
 import io.zeroparadigm.liquid.core.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,8 +36,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.apache.shiro.subject.Subject;
 
 /**
  * Fetch and modify user info. Register user.
@@ -66,14 +61,14 @@ public class UserController {
      */
     @ApiOperation(value = "hello", notes = "says hello to the person")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "name", value = "USER_NAME", required = true, dataTypeClass = String.class, example = "chris")
+        @ApiImplicitParam(name = "name", value = "USER_NAME", required = true, dataTypeClass = String.class, example = "chris")
     })
     @PostMapping(value = "/hello")
     @ResponseStatus(HttpStatus.CREATED)
     @WrapsException(ServiceStatus.REQUEST_PARAMS_NOT_VALID_ERROR)
     public Result<String> hello(
-                                @RequestParam String name,
-                                @RequestBody(required = false) String ts) {
+        @RequestParam String name,
+        @RequestBody(required = false) String ts) {
         log.debug("User {}", name);
         return Result.success("userService.greet(name)");
     }
@@ -86,23 +81,13 @@ public class UserController {
      */
     @ApiOperation(value = "getUserNamedAs", notes = "queries the first user with that name")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "name", value = "user's name", required = true, dataTypeClass = String.class, example = "foo")
+        @ApiImplicitParam(name = "name", value = "user's name", required = true, dataTypeClass = String.class, example = "foo")
     })
     @PostMapping(value = "/user")
     @ResponseStatus(HttpStatus.OK)
     @WrapsException(value = ServiceStatus.INTERNAL_SERVER_ERROR_ARGS, status = HttpStatus.NOT_ACCEPTABLE)
     public Result<User> getUserNamedAs(@RequestParam(value = "name") String name) {
         throw new RuntimeException();
-    }
-
-    @PostMapping("/getCredential")
-    public Result<JSONObject> getCredential() {
-        Subject subject = SecurityUtils.getSubject();
-        Result<JSONObject> result = Result.success();
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("result", subject.getPrincipal());
-        result.setData(jsonObject);
-        return result;
     }
 
 }
